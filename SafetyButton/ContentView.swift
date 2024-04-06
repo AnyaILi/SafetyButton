da67e9ed6a35d4d2ef4a0d7faebaf972d128e937
Anya <anyali0219@gmail.com>
2024-04-05 22:33:08 -0700

    Initial Commit

---
+++ SafetyButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafetyButton
+//
+//  Created by Anya Li on 4/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafetyButton/SafetyButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyButtonApp.swift
+//  SafetyButton
+//
+//  Created by Anya Li on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SafetyButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
